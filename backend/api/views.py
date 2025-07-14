10abc6dc5d87e23f2cf1875c817ad6458bc4ceff
tobywuST <tobywu0103@yahoo.com.tw>
2025-07-14 19:53:29 +0800

    Revert "Merge pull request #20 from TobyWuNumOne/develop"
    This reverts commit d9e7504a6e87142c1091b0de36c3da0b13f6f31f, reversing
    changes made to 371220bac3e305e67b7d1edc9cc5504a011f864f.

---
+++ backend/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(["GET"])
+def test_api(request):
+    return Response({"message": "Django REST Framework is working!"})
